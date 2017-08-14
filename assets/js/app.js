cb0c837e9becd9955c57f0bd1795a1214f3f9adb
Joseph Pulaski <GoodPBC@users.noreply.github.com>
2017-08-14 15:08:41 -0400

    Updated typed.js for es6

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,12 +6,19 @@ $(document).ready(function(){
 });
 
 //type animation
-$(function(){
-    $("#animate-type").typed({
-        strings: ["<strong style='color: red'>a lover</strong>. ^500", "<strong style='color: red'>a dreamer</strong>. ^500", "<strong style='color: red'>a father</strong>. ^500", "<strong style='color: red'>a fiance</strong>. ^500", "<strong style='color: red'>a creator</strong>. ^500", "<strong style='color: red'>an innovator</strong>. ^500", "<strong style='color: red'>an entrepreneur</strong>. ^500", "<strong style='color: red'>a student</strong>. ^500", "<strong style='color: red'>a leader</strong>. ^500", "<strong style='color: red'>a friend</strong>. ^500", "<strong style='color: red'>a believer</strong>. ^500", "<strong style='color: red'>a thinker</strong>. ^500", "<strong style='color: red'>a doer</strong>. ^500", "<strong style='color: red'>authentic</strong>. ^500", "<strong style='color: red'>empathetic</strong>. ^500", "<strong style='color: red'>a man of integrity</strong>. ^500", "<strong style='color: red'>reliable</strong>. ^500", "<strong style='color: red'>exceptional</strong>. ^500", "<strong style='color: red'>humble</strong>. ^500", "<strong style='color: red'>grateful</strong>. ^500", "<strong style='color: red'>HUMAN</strong>. ^500", "<strong style='color: red'>READY</strong>. ^500"],
-        contentType: 'html',
-        typeSpeed: 0,
-        shuffle: true,
-    });
+// $(function(){
+//     $("#animate-type").typed({
+//         strings: ["<strong style='color: red'>a lover</strong>. ^500", "<strong style='color: red'>a dreamer</strong>. ^500", "<strong style='color: red'>a father</strong>. ^500", "<strong style='color: red'>a fiance</strong>. ^500", "<strong style='color: red'>a creator</strong>. ^500", "<strong style='color: red'>an innovator</strong>. ^500", "<strong style='color: red'>an entrepreneur</strong>. ^500", "<strong style='color: red'>a student</strong>. ^500", "<strong style='color: red'>a leader</strong>. ^500", "<strong style='color: red'>a friend</strong>. ^500", "<strong style='color: red'>a believer</strong>. ^500", "<strong style='color: red'>a thinker</strong>. ^500", "<strong style='color: red'>a doer</strong>. ^500", "<strong style='color: red'>authentic</strong>. ^500", "<strong style='color: red'>empathetic</strong>. ^500", "<strong style='color: red'>a man of integrity</strong>. ^500", "<strong style='color: red'>reliable</strong>. ^500", "<strong style='color: red'>exceptional</strong>. ^500", "<strong style='color: red'>humble</strong>. ^500", "<strong style='color: red'>grateful</strong>. ^500", "<strong style='color: red'>HUMAN</strong>. ^500", "<strong style='color: red'>READY</strong>. ^500"],
+//         contentType: 'html',
+//         typeSpeed: 0,
+//         shuffle: true,
+//     });
+// });
+
+var typed = new Typed("#animate-type", {
+  strings: ["<strong style='color: red'>a lover</strong>. ^500", "<strong style='color: red'>a dreamer</strong>. ^500", "<strong style='color: red'>a father</strong>. ^500", "<strong style='color: red'>a fiance</strong>. ^500", "<strong style='color: red'>a creator</strong>. ^500", "<strong style='color: red'>an innovator</strong>. ^500", "<strong style='color: red'>an entrepreneur</strong>. ^500", "<strong style='color: red'>a student</strong>. ^500", "<strong style='color: red'>a leader</strong>. ^500", "<strong style='color: red'>a friend</strong>. ^500", "<strong style='color: red'>a believer</strong>. ^500", "<strong style='color: red'>a thinker</strong>. ^500", "<strong style='color: red'>a doer</strong>. ^500", "<strong style='color: red'>authentic</strong>. ^500", "<strong style='color: red'>empathetic</strong>. ^500", "<strong style='color: red'>a man of integrity</strong>. ^500", "<strong style='color: red'>reliable</strong>. ^500", "<strong style='color: red'>exceptional</strong>. ^500", "<strong style='color: red'>humble</strong>. ^500", "<strong style='color: red'>grateful</strong>. ^500", "<strong style='color: red'>HUMAN</strong>. ^500", "<strong style='color: red'>READY</strong>. ^500"],
+  typeSpeed: 30,
+  contentType: 'html',
+  shuffle: true,  
 });
 
